1891916637be0db8b1ba3c8fcf17cbadde9410d7
dennismachel <dennis.machel@gmail.com>
2024-02-19 22:26:06 -0500

    initial committed

--- teste.py
+++ teste.py
@@ -1,3 +1,10 @@
+#Escreva um programa em Python que solicita ao usuário 
+#para digitar seu nome, o valor do seu salário mensal e 
+#o valor do bônus que recebeu. 
+#O programa deve, então, imprimir uma mensagem saudando o 
+#usuário pelo nome e informando o valor do salário 
+#em comparação com o bônus recebido.#
+
 nome = input("Digite seu nome: ")
 valor_salario = int(input("Digite o seu salario: "))
 bonus_porcentagem = 1.5
